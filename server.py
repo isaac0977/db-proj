21856a5384fa23bcab883e637366164966208dd9
Isaac Suh <isuh@Isaacs-MacBook-Pro.local>
2021-11-10 21:12:59 -0500

    added multi

--- server.py
+++ server.py
@@ -23,17 +23,6 @@ tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
 
 
-#
-# The following is a dummy URI that does not connect to a valid database. You will need to modify it to connect to your Part 2 database in order to use the data.
-#
-# XXX: The URI should be in the format of: 
-#
-#     postgresql://USER:PASSWORD@104.196.152.219/proj1part2
-#
-# For example, if you had username biliris and password foobar, then the following line would be:
-#
-#     DATABASEURI = "postgresql://biliris:foobar@104.196.152.219/proj1part2"
-#
 DATABASEURI = "postgresql://is2661:7399@35.196.73.133/proj1part2"
 
 
@@ -42,15 +31,7 @@ DATABASEURI = "postgresql://is2661:7399@35.196.73.133/proj1part2"
 #
 engine = create_engine(DATABASEURI)
 
-#
-# Example of running queries in your database
-# Note that this will probably not work if you already have a table named 'test' in your database, containing meaningful data. This is only an example showing you how to run queries in your database using SQLAlchemy.
-# #
-# engine.execute("""CREATE TABLE IF NOT EXISTS test (
-#   id serial,
-#   name text
-# );""")
-# engine.execute("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace');""")
+
 
 def rename_keys(row):
   if ('x_coordinates' in row):
@@ -88,23 +69,6 @@ def teardown_request(exception):
     pass
 
 
-#
-# @app.route is a decorator around index() that means:
-#   run index() whenever the user tries to access the "/" path using a GET request
-#
-# If you wanted the user to go to, for example, localhost:8111/foobar/ with POST or GET then you could use:
-#
-#       @app.route("/foobar/", methods=["POST", "GET"])
-#
-# PROTIP: (the trailing / in the path is important)
-# 
-# see for routing: http://flask.pocoo.org/docs/0.10/quickstart/#routing
-# see for decorators: http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
-#
-
-
-
-
 
 
 def construct_single_query(tableName, statName, year):
@@ -134,19 +98,21 @@ def construct_single_query(tableName, statName, year):
 
   return query;
 
+def construct_multi_query(table1, table2, stat1, stat2, year):
+  query1 = construct_single_query(table1, stat1, year)
+
+  query2 = construct_single_query(table2, stat2, year)
+
+  combinedQuery = '''
+    SELECT query1.city_id, query1.x_coordinates, query1.y_coordinates, query1.city_name, query1.size as data1, query2.size as data2, query1.size/query2.size as size
+    from ({}) query1, ({}) query2 where query1.city_id=query2.city_id'''.format(query1, query2)
+  return combinedQuery
+
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-  """
-  request is a special object that Flask provides to access web request information:
 
-  request.method:   "GET" or "POST"
-  request.form:     if the browser submitted a form, this contains the data in the form
-  request.args:     dictionary of URL arguments, e.g., {a:1, b:2} for http://localhost?a=1&b=2
-
-  See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
-  """
 
   print(request.form)
 
@@ -200,29 +166,9 @@ def index():
         query = construct_single_query(request.form['macrostat-table'], request.form[statNameKey], int(request.form['year']))
         print(query)
 
-        if ('macrostat2' in request.form and request.form['macrostat2'] != ''):
-            if (request.form['macrostat2'] == 'avg_unemployment_rate'):
-    
-              query = '''
-              SELECT t.city_id, city_name, x_coordinates, y_coordinates, gross_earning as data1, avg_unemployment_rate data2, gross_earning/avg_unemployment_rate as size
-              from city 
-              INNER JOIN
-              (
-                SELECT c.city_id, gross_earning, avg_unemployment_rate
-                FROM 
-                  general_compensation
-                  INNER JOIN (
-                    SELECT 
-                    city_id,
-                    year,
-                    avg_unemployment_rate
-                  FROM 
-                    employment
-                  WHERE
-                    year={}
-                ) as c on general_compensation.city_id = c.city_id and general_compensation.year = c.year
-                ) as t on t.city_id = city.city_id
-              '''.format(int(request.form['year']))
+        if (request.form['queryType'] == 'multi'):
+            statNameKey2 = 'macrostat-{}'.format(request.form['macrostat-table2'])
+            query = construct_multi_query(request.form['macrostat-table'], request.form['macrostat-table2'], request.form[statNameKey], request.form[statNameKey2], int(request.form['year']))
 
         
         print(query)
@@ -242,14 +188,7 @@ def index():
 
   return render_template("index.html", **context)
 
-#
-# This is an example of a different path.  You can see it at:
-# 
-#     localhost:8111/another
-#
-# Notice that the function name is another() rather than index()
-# The functions for each app.route need to have different names
-#
+
 @app.route('/another')
 def another():
   return render_template("another.html")
